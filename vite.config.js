eb152753fcad7d58c1e30d5a8d3088c40de568a8
im'prEss <313069365@qq.com>
2026-05-04 04:19:49 +0800

    0.2.18 fix:

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-import { cloudflare } from "@cloudflare/vite-plugin";
+// import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
